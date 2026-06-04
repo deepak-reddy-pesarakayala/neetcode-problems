3072371b34332b35e539aff81f055b5d2487d104
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-06-04 16:13:26 +0530

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int count = 0;
+
+        for (String passenger : details) {
+            int age = Integer.parseInt(passenger.substring(11, 13));
+
+            if (age > 60) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
