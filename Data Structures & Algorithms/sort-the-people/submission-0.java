4f7cd4a2b7ebdf80355945cd253e33ad64458109
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-06-06 23:03:46 +0530

    Add: sort-the-people - submission-0

---
+++ Data Structures & Algorithms/sort-the-people/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public String[] sortPeople(String[] names, int[] heights) {
+
+        Integer[] indices = new Integer[heights.length];
+
+        for (int i = 0; i < heights.length; i++) {
+            indices[i] = i;
+        }
+
+        Arrays.sort(indices, (a, b) -> heights[b] - heights[a]);
+
+        String[] result = new String[names.length];
+
+        for (int i = 0; i < indices.length; i++) {
+            result[i] = names[indices[i]];
+        }
+
+        return result;
+    }
+}
