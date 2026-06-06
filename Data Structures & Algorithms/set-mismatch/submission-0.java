54064746af7d89a46b737489494a12255a281182
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-06-06 22:55:51 +0530

    Add: set-mismatch - submission-0

---
+++ Data Structures & Algorithms/set-mismatch/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public int[] findErrorNums(int[] nums) {
+
+        int n = nums.length;
+        int[] freq = new int[n + 1];
+
+        int duplicate = 0;
+        int missing = 0;
+
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        for (int i = 1; i <= n; i++) {
+
+            if (freq[i] == 2) {
+                duplicate = i;
+            }
+
+            if (freq[i] == 0) {
+                missing = i;
+            }
+        }
+
+        return new int[]{duplicate, missing};
+    }
+}
