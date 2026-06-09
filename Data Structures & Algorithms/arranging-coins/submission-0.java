4710d52f982f55928d2744cc063cac31916093ce
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-06-09 20:11:16 +0530

    Add: arranging-coins - submission-0

---
+++ Data Structures & Algorithms/arranging-coins/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        long left = 1;
+        long right = n;
+        long answer = 0;
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long coinsNeeded = mid * (mid + 1) / 2;
+            if (coinsNeeded <= n) {
+                answer = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return (int) answer;
+    }
+}
