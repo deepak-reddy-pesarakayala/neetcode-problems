bb67b6116c1eac7a943502b45f181898b8e7a500
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-06-14 09:20:20 +0530

    Add: minimum-time-to-collect-all-apples-in-a-tree - submission-0

---
+++ Data Structures & Algorithms/minimum-time-to-collect-all-apples-in-a-tree/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+        return dfs(0, -1, graph, hasApple);
+    }
+    private int dfs(int node, int parent, List<List<Integer>> graph, List<Boolean> hasApple) {
+        int time = 0;
+        for (int child : graph.get(node)) {
+            if (child == parent) continue;
+            int childTime = dfs(child, node, graph, hasApple);
+            if (childTime > 0 || hasApple.get(child)) {
+                time += childTime + 2;
+            }
+        }
+        return time;
+    }
+}
+    
