f0c51fb196ece46b4f27a970a430b640dd167db3
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-07-06 16:57:51 +0530

    Add: non-cyclical-number - submission-1

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-1.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {boolean}
+     */
+    isHappy(n) {
+        const visit = new Set();
+        while (!visit.has(n)) {
+            visit.add(n);
+            n = this.sumOfSquares(n);
+            if (n === 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    sumOfSquares(n) {
+        let output = 0;
+        while (n > 0) {
+            let digit = n % 10;
+            digit = digit * digit;
+            output += digit;
+            n = Math.floor(n / 10);
+        }
+        return output;
+    }
+}
