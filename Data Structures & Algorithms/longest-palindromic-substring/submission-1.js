fb1133a8de52165db9395c93de9f35c13aeddf87
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-07-07 19:29:57 +0530

    Add: longest-palindromic-substring - submission-1

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-1.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {string}
+     */
+    longestPalindrome(s) {
+        let res = '';
+        let resLen = 0;
+        for (let i = 0; i < s.length; i++) {
+            for (let j = i; j < s.length; j++) {
+                let l = i,
+                    r = j;
+                while (l < r && s[l] === s[r]) {
+                    l++;
+                    r--;
+                }
+                if (l >= r && resLen < j - i + 1) {
+                    res = s.slice(i, j + 1);
+                    resLen = j - i + 1;
+                }
+            }
+        }
+        return res;
+    }
+}
