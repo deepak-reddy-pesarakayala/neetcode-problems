7e5befc652cea9488749a3d656360da5f2911052
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-07-07 19:58:57 +0530

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.js
@@ -0,0 +1,13 @@
+class Solution {
+    climbStairs(n) {
+        if (n <= 2) return n;
+        let a = 1;
+        let b = 2;
+        for (let i = 3; i <= n; i++) {
+            let c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+}
