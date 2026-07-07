7a06e009a7c2af3dea10cf963a22bc089176404d
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-07-07 19:31:34 +0530

    Add: palindromic-substrings - submission-1

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    countSubstrings(s) {
+        let res = 0;
+        for (let i = 0; i < s.length; i++) {
+            for (let j = i; j < s.length; j++) {
+                let l = i,
+                    r = j;
+                while (l < r && s[l] === s[r]) {
+                    l++;
+                    r--;
+                }
+                res += l >= r ? 1 : 0;
+            }
+        }
+        return res;
+    }
+}
