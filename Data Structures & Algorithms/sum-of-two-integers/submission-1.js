5e4673425b8cba8a2ea896a6af173132388bd35b
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-07-08 19:35:32 +0530

    Add: sum-of-two-integers - submission-1

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-1.js
@@ -0,0 +1,10 @@
+class Solution {
+    getSum(a, b) {
+        while (b !== 0) {
+            let carry = (a & b) << 1;
+            a = a ^ b;
+            b = carry;
+        }
+        return a;
+    }
+}
