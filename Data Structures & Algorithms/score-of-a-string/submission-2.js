a5cf6cf41e5c0161b00693cc41bfe372ced27524
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-07-08 19:42:44 +0530

    Add: score-of-a-string - submission-2

---
+++ Data Structures & Algorithms/score-of-a-string/submission-2.js
@@ -0,0 +1,9 @@
+class Solution {
+    scoreOfString(s) {
+        let score = 0;
+        for (let i = 1; i < s.length; i++) {
+            score += Math.abs(s.charCodeAt(i) - s.charCodeAt(i - 1));
+        }
+        return score;
+    }
+}
