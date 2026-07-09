3a3a594df9188b0347d27a4ec32a596a232397b4
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-07-09 19:13:27 +0530

    Add: pascals-triangle - submission-1

---
+++ Data Structures & Algorithms/pascals-triangle/submission-1.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number} numRows
+     * @return {number[][]}
+     */
+    generate(numRows) {
+        const result = [];
+        for (let i = 0; i < numRows; i++) {
+            result[i] = [];
+            for (let j = 0; j <= i; j++) {
+                if (j === 0 || j === i) {
+                    result[i][j] = 1;
+                } else {
+                    result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
+                }
+            }
+        }
+        return result;
+    }
+}
