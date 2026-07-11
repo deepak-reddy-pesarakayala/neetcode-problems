93b186e104b83ee93ea6527b4ed13c9fa8bbe5c1
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-07-11 11:26:48 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.js
@@ -0,0 +1,30 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    push(val) {
+        this.stack.push(val);
+
+        if (this.minStack.length === 0 || val <= this.getMin()) {
+            this.minStack.push(val);
+        }
+    }
+
+    pop() {
+        const val = this.stack.pop();
+
+        if (val === this.getMin()) {
+            this.minStack.pop();
+        }
+    }
+
+    top() {
+        return this.stack[this.stack.length - 1];
+    }
+
+    getMin() {
+        return this.minStack[this.minStack.length - 1];
+    }
+}
