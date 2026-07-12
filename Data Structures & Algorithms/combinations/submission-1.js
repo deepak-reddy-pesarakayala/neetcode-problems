5c4992e71a94783d8438b619f1f7d7adfae49007
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-07-12 08:45:45 +0530

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.js
@@ -0,0 +1,18 @@
+class Solution {
+    combine(n, k) {
+        const result = [];
+        const backtrack = (start, current) => {
+            if (current.length === k) {
+                result.push([...current]);
+                return;
+            }
+            for (let i = start; i <= n; i++) {
+                current.push(i);
+                backtrack(i + 1, current);
+                current.pop();
+            }
+        };
+        backtrack(1, []);
+        return result;
+    }
+}
