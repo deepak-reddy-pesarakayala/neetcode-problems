dd1aae6bf65deff39b2a9c4a0c3f9ad51e9434c7
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-07-12 09:06:18 +0530

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.js
@@ -0,0 +1,31 @@
+class Solution {
+    permute(nums) {
+        const result = [];
+        const used = new Array(nums.length).fill(false);
+
+        const backtrack = (current) => {
+            if (current.length === nums.length) {
+                result.push([...current]);
+                return;
+            }
+
+            for (let i = 0; i < nums.length; i++) {
+                if (used[i]) {
+                    continue;
+                }
+
+                used[i] = true;
+                current.push(nums[i]);
+
+                backtrack(current);
+
+                current.pop();
+                used[i] = false;
+            }
+        };
+
+        backtrack([]);
+
+        return result;
+    }
+}
