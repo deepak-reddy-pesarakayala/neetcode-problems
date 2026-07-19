46447aa974a9a7b77c7ff87455c7c1faefdd16ac
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-07-19 15:20:47 +0530

    Add: word-pattern - submission-1

---
+++ Data Structures & Algorithms/word-pattern/submission-1.js
@@ -0,0 +1,23 @@
+class Solution {
+    wordPattern(pattern, s) {
+        const words = s.split(" ");
+        if (pattern.length !== words.length) {
+            return false;
+        }
+        const charToWord = new Map();
+        const wordToChar = new Map();
+        for (let i = 0; i < pattern.length; i++) {
+            const ch = pattern[i];
+            const word = words[i];
+            if (charToWord.has(ch) && charToWord.get(ch) !== word) {
+                return false;
+            }
+            if (wordToChar.has(word) && wordToChar.get(word) !== ch) {
+                return false;
+            }
+            charToWord.set(ch, word);
+            wordToChar.set(word, ch);
+        }
+        return true;
+    }
+}
