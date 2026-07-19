508616c510b8a837a583973b62c000ef1f370f30
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-07-19 15:27:04 +0530

    Add: special-array-i - submission-1

---
+++ Data Structures & Algorithms/special-array-i/submission-1.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    isArraySpecial(nums) {
+        for (let i = 1; i < nums.length; i++) {
+            if ((nums[i] % 2) === (nums[i - 1] % 2)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
