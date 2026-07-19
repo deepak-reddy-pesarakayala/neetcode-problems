72926a0ea512e52198ec610865c2842b7029a68d
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-07-19 15:29:06 +0530

    Add: monotonic-array - submission-1

---
+++ Data Structures & Algorithms/monotonic-array/submission-1.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    isMonotonic(nums) {
+        let increasing = true;
+        let decreasing = true;
+        for (let i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                decreasing = false;
+            }
+            if (nums[i] < nums[i - 1]) {
+                increasing = false;
+            }
+        }
+        return increasing || decreasing;
+    }
+}
