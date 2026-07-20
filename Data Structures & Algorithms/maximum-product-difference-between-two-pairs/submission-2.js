93504a4ebd223e076782579ec9d3f580909efd12
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-07-20 17:29:13 +0530

    Add: maximum-product-difference-between-two-pairs - submission-2

---
+++ Data Structures & Algorithms/maximum-product-difference-between-two-pairs/submission-2.js
@@ -0,0 +1,11 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxProductDifference(nums) {
+        nums.sort((a, b) => a - b);
+        const n = nums.length;
+        return nums[n - 1] * nums[n - 2] - nums[0] * nums[1];
+    }
+}
