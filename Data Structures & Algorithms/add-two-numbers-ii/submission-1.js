46722ef2430e8d1372a84fbdf0022a2c87b102f2
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-07-22 16:46:03 +0530

    Add: add-two-numbers-ii - submission-1

---
+++ Data Structures & Algorithms/add-two-numbers-ii/submission-1.js
@@ -0,0 +1,26 @@
+class Solution {
+    addTwoNumbers(l1, l2) {
+        const s1 = [];
+        const s2 = [];
+        while (l1) {
+            s1.push(l1.val);
+            l1 = l1.next;
+        }
+        while (l2) {
+            s2.push(l2.val);
+            l2 = l2.next;
+        }
+        let carry = 0;
+        let head = null;
+        while (s1.length || s2.length || carry) {
+            let sum = carry;
+            if (s1.length) sum += s1.pop();
+            if (s2.length) sum += s2.pop();
+            carry = Math.floor(sum / 10);
+            const node = new ListNode(sum % 10);
+            node.next = head;
+            head = node;
+        }
+        return head;
+    }
+}
