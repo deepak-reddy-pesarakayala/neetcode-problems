2837d8ef7942c97c05e51d987689df24e80bda98
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-07-22 16:49:05 +0530

    Add: reverse-linked-list-ii - submission-1

---
+++ Data Structures & Algorithms/reverse-linked-list-ii/submission-1.js
@@ -0,0 +1,19 @@
+class Solution {
+    reverseBetween(head, left, right) {
+        if (!head || left === right) return head;
+        const dummy = new ListNode(0);
+        dummy.next = head;
+        let prev = dummy;
+        for (let i = 1; i < left; i++) {
+            prev = prev.next;
+        }
+        let curr = prev.next;
+        for (let i = 0; i < right - left; i++) {
+            let next = curr.next;
+            curr.next = next.next;
+            next.next = prev.next;
+            prev.next = next;
+        }
+        return dummy.next;
+    }
+}
