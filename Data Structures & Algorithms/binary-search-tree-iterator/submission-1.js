1bb533771c6e44e0639c7b41384b325e2e8fa22e
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-07-23 14:35:29 +0530

    Add: binary-search-tree-iterator - submission-1

---
+++ Data Structures & Algorithms/binary-search-tree-iterator/submission-1.js
@@ -0,0 +1,22 @@
+class BSTIterator {
+    constructor(root) {
+        this.stack = [];
+        this.pushLeft(root);
+    }
+    pushLeft(node) {
+        while (node) {
+            this.stack.push(node);
+            node = node.left;
+        }
+    }
+    next() {
+        const node = this.stack.pop();
+        if (node.right) {
+            this.pushLeft(node.right);
+        }
+        return node.val;
+    }
+    hasNext() {
+        return this.stack.length > 0;
+    }
+}
