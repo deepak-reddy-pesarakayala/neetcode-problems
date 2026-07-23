f7a9b3e6928ffed2c861bf838fb1c2730a2fbcdb
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-07-23 15:04:30 +0530

    Add: find-the-town-judge - submission-1

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-1.js
@@ -0,0 +1,15 @@
+class Solution {
+    findJudge(n, trust) {
+        const score = new Array(n + 1).fill(0);
+        for (const [a, b] of trust) {
+            score[a]--;
+            score[b]++;
+        }
+        for (let i = 1; i <= n; i++) {
+            if (score[i] === n - 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
