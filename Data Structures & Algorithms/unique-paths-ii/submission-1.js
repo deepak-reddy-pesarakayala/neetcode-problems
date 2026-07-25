406a2a020d33093c47806e365b836589e38c783d
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-07-25 15:27:52 +0530

    Add: unique-paths-ii - submission-1

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-1.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[][]} obstacleGrid
+     * @return {number}
+     */
+    uniquePathsWithObstacles(obstacleGrid) {
+        const m = obstacleGrid.length;
+        const n = obstacleGrid[0].length;
+        if (obstacleGrid[0][0] === 1 || obstacleGrid[m - 1][n - 1] === 1) {
+            return 0;
+        }
+        const dp = new Array(n).fill(0);
+        dp[0] = 1;
+        for (let i = 0; i < m; i++) {
+            for (let j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] === 1) {
+                    dp[j] = 0;
+                } else if (j > 0) {
+                    dp[j] += dp[j - 1];
+                }
+            }
+        }
+        return dp[n - 1];
+    }
+}
