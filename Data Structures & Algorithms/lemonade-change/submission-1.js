955087823bd39d0f8a46bbd4466ce96783ebb151
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-07-25 16:10:10 +0530

    Add: lemonade-change - submission-1

---
+++ Data Structures & Algorithms/lemonade-change/submission-1.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} bills
+     * @return {boolean}
+     */
+    lemonadeChange(bills) {
+        let five = 0;
+        let ten = 0;
+        for (const bill of bills) {
+            if (bill === 5) {
+                five++;
+            } else if (bill === 10) {
+                if (five === 0) return false;
+                five--;
+                ten++;
+            } else {
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
