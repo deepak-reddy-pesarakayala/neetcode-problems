b7fb92c24decdecf3c634fd6aba55eefdebe49c6
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-08-19 20:56:02 +0530

    Add: permutations-ii - submission-2

---
+++ Data Structures & Algorithms/permutations-ii/submission-2.kt
@@ -0,0 +1,27 @@
+class Solution {
+    fun permuteUnique(nums: IntArray): List<List<Int>> {
+        nums.sort()
+        val result = mutableListOf<List<Int>>()
+        val current = mutableListOf<Int>()
+        val used = BooleanArray(nums.size)
+        fun backtrack() {
+            if (current.size == nums.size) {
+                result.add(current.toList())
+                return
+            }
+            for (i in nums.indices) {
+                if (used[i]) continue
+                if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
+                    continue
+                }
+                used[i] = true
+                current.add(nums[i])
+                backtrack()
+                current.removeAt(current.size - 1)
+                used[i] = false
+            }
+        }
+        backtrack()
+        return result
+    }
+}
