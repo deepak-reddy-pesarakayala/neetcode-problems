01ab7783af976a2fe837bc97a0390cfe3952d046
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-08-19 20:31:19 +0530

    Add: delete-leaves-with-a-given-value - submission-2

---
+++ Data Structures & Algorithms/delete-leaves-with-a-given-value/submission-2.kt
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun removeLeafNodes(root: TreeNode?, target: Int): TreeNode? {
+        if (root == null) return null
+        root.left = removeLeafNodes(root.left, target)
+        root.right = removeLeafNodes(root.right, target)
+        if (root.left == null && root.right == null && root.`val` == target) {
+            return null
+        }
+        return root
+    }
+}
