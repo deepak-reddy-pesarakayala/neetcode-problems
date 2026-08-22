570f07099e34d5ccca91b4a694901b092f8bfcc9
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-08-22 15:36:09 +0530

    Add: combination-sum-iv - submission-2

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-2.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun combinationSum4(nums: IntArray, target: Int): Int {
+        val dp = IntArray(target + 1)
+        dp[0] = 1
+        for (i in 1..target) {
+            for (num in nums) {
+                if (num <= i) {
+                    dp[i] += dp[i - num]
+                }
+            }
+        }
+        return dp[target]
+    }
+}
