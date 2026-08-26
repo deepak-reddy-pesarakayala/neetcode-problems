86ce01c81b6a7a53af0a228a68f2b81388d3d064
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-08-26 15:36:15 +0530

    Add: integer-to-roman - submission-2

---
+++ Data Structures & Algorithms/integer-to-roman/submission-2.kt
@@ -0,0 +1,24 @@
+class Solution {
+    fun intToRoman(num: Int): String {
+        val values = intArrayOf(
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        )
+        val symbols = arrayOf(
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV", "I"
+        )
+        var number = num
+        val result = StringBuilder()
+        for (i in values.indices) {
+            while (number >= values[i]) {
+                result.append(symbols[i])
+                number -= values[i]
+            }
+        }
+
+        return result.toString()
+    }
+}
