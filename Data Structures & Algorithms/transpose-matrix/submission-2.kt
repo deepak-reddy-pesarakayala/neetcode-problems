e19dca26fbee1e1fc474e5d521e523ee6490e046
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-08-26 15:20:07 +0530

    Add: transpose-matrix - submission-2

---
+++ Data Structures & Algorithms/transpose-matrix/submission-2.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun transpose(matrix: Array<IntArray>): Array<IntArray> {
+        val m = matrix.size
+        val n = matrix[0].size
+        val result = Array(n) { IntArray(m) }
+        for (i in 0 until m) {
+            for (j in 0 until n) {
+                result[j][i] = matrix[i][j]
+            }
+        }
+        return result
+    }
+}
