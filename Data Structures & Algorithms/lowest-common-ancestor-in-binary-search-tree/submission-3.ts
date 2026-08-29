2f1f6322e4f2645d266b9a292f5d2de0e6e845d9
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-08-29 21:33:26 +0530

    Add: lowest-common-ancestor-in-binary-search-tree - submission-3

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-3.ts
@@ -0,0 +1,20 @@
+class Solution {
+    lowestCommonAncestor(
+        root: TreeNode | null,
+        p: TreeNode,
+        q: TreeNode
+    ): TreeNode | null {
+        let current = root;
+        while (current !== null) {
+            if (p.val < current.val && q.val < current.val) {
+                current = current.left;
+            } else if (p.val > current.val && q.val > current.val) {
+                current = current.right;
+            } else {
+                return current;
+            }
+        }
+        return null;
+    }
+}
+
