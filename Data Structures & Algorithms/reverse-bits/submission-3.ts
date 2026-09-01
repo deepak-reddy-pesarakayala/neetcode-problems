a6be76399b3cf7c88a267cd972e33f60f411167b
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-09-01 15:19:33 +0530

    Add: reverse-bits - submission-3

---
+++ Data Structures & Algorithms/reverse-bits/submission-3.ts
@@ -0,0 +1,10 @@
+class Solution {
+    reverseBits(n: number): number {
+        let res = 0;
+        for (let i = 0; i < 32; i++) {
+            res = (res << 1) | (n & 1);
+            n >>>= 1;
+        }
+        return res >>> 0;
+    }
+}
