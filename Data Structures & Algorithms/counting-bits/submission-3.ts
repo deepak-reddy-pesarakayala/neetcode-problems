29922d2cfd128981f9fcb59a2ccc5e23975ef30d
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-09-01 15:17:13 +0530

    Add: counting-bits - submission-3

---
+++ Data Structures & Algorithms/counting-bits/submission-3.ts
@@ -0,0 +1,9 @@
+class Solution {
+    countBits(n: number): number[] {
+        const dp: number[] = new Array(n + 1).fill(0);
+        for (let i = 1; i <= n; i++) {
+            dp[i] = dp[i >> 1] + (i & 1);
+        }
+        return dp;
+    }
+}
