eeaf99ffcc8c9bba1a706266e68b6a700582e90e
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-09-01 15:21:37 +0530

    Add: missing-number - submission-3

---
+++ Data Structures & Algorithms/missing-number/submission-3.ts
@@ -0,0 +1,9 @@
+class Solution {
+    missingNumber(nums: number[]): number {
+        let res = nums.length;
+        for (let i = 0; i < nums.length; i++) {
+            res ^= i ^ nums[i];
+        }
+        return res;
+    }
+}
