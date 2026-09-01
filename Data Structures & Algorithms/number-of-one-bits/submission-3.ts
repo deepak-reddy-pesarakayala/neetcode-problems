6eed3f6f7d65e9db2c15131ef5e463e3ced0f6e8
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-09-01 15:15:05 +0530

    Add: number-of-one-bits - submission-3

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-3.ts
@@ -0,0 +1,10 @@
+class Solution {
+    hammingWeight(n: number): number {
+        let count = 0;
+        while (n !== 0) {
+            n &= n - 1;
+            count++;
+        }
+        return count;
+    }
+}
