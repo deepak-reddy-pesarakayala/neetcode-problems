1909a694705a94178473fb2ba0abe4c733450e94
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-09-01 15:27:24 +0530

    Add: bitwise-and-of-numbers-range - submission-3

---
+++ Data Structures & Algorithms/bitwise-and-of-numbers-range/submission-3.ts
@@ -0,0 +1,11 @@
+class Solution {
+    rangeBitwiseAnd(left: number, right: number): number {
+        let shifts = 0;
+        while (left !== right) {
+            left >>= 1;
+            right >>= 1;
+            shifts++;
+        }
+        return left << shifts;
+    }
+}
