81c207614c0dbd32dcc163e45a1c4e0384ef7bd9
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-09-04 19:13:43 +0530

    Add: valid-binary-search-tree - submission-4

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-4.go
@@ -0,0 +1,15 @@
+func isValidBST(root *TreeNode) bool {
+	return validate(root, nil, nil)
+}
+func validate(node *TreeNode, min *int, max *int) bool {
+	if node == nil {
+		return true
+	}
+	if min != nil && node.Val <= *min {
+		return false
+	}
+	if max != nil && node.Val >= *max {
+		return false
+	}
+	return validate(node.Left, min, &node.Val) && validate(node.Right, &node.Val, max)
+}
