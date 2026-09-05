2b91ffca5868b86d577908eb7a9376e2335c3a07
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-09-05 19:18:06 +0530

    Add: word-break - submission-4

---
+++ Data Structures & Algorithms/word-break/submission-4.go
@@ -0,0 +1,18 @@
+func wordBreak(s string, wordDict []string) bool {
+	n := len(s)
+	wordSet := make(map[string]bool)
+    for _, word := range wordDict {
+		wordSet[word] = true
+	}
+    dp := make([]bool, n+1)
+	dp[0] = true
+    for i := 1; i <= n; i++ {
+		for j := 0; j < i; j++ {
+			if dp[j] && wordSet[s[j:i]] {
+				dp[i] = true
+				break
+			}
+		}
+	} 
+	return dp[n]
+}
