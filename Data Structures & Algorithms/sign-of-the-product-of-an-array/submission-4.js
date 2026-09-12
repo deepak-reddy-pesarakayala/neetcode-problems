5211d3097d7d448dfc5977fb32d5f88572e89e73
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-09-12 19:53:55 +0530

    Add: sign-of-the-product-of-an-array - submission-4

---
+++ Data Structures & Algorithms/sign-of-the-product-of-an-array/submission-4.js
@@ -0,0 +1,17 @@
+class Solution {
+    arraySign(nums) {
+        let negativeCount = 0;
+
+        for (let num of nums) {
+            if (num === 0) {
+                return 0;
+            }
+
+            if (num < 0) {
+                negativeCount++;
+            }
+        }
+
+        return negativeCount % 2 === 0 ? 1 : -1;
+    }
+}
