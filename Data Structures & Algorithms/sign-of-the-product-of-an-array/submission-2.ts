5d41d01d9d56f389a209ad04b095f1fb6e7ea9dd
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-09-12 12:15:35 +0530

    Add: sign-of-the-product-of-an-array - submission-2

---
+++ Data Structures & Algorithms/sign-of-the-product-of-an-array/submission-2.ts
@@ -0,0 +1,10 @@
+class Solution {
+    arraySign(nums: number[]): number {
+        let negative = 0;
+        for (const num of nums) {
+            if (num === 0) return 0;
+            if (num < 0) negative++;
+        }
+        return negative % 2 === 0 ? 1 : -1;
+    }
+}
