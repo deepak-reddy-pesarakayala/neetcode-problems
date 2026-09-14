0524d9ef051ad4ccbaaed9672176899f5d896c87
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-09-14 20:46:54 +0530

    Add: remove-duplicates-from-sorted-array - submission-5

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-5.ts
@@ -0,0 +1,12 @@
+class Solution {
+    removeDuplicates(nums: number[]): number {
+        let k = 1;
+        for (let i = 1; i < nums.length; i++) {
+            if (nums[i] !== nums[k - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
