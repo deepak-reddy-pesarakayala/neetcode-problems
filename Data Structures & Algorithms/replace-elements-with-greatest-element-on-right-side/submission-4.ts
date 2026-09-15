bb6931e2448a79125c5a331e9c64912d24e2a279
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-09-15 20:40:53 +0530

    Add: replace-elements-with-greatest-element-on-right-side - submission-4

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-4.ts
@@ -0,0 +1,11 @@
+class Solution {
+    replaceElements(arr: number[]): number[] {
+        let maxRight = -1;
+        for (let i = arr.length - 1; i >= 0; i--) {
+            const current = arr[i];
+            arr[i] = maxRight;
+            maxRight = Math.max(maxRight, current);
+        }
+        return arr;
+    }
+}
