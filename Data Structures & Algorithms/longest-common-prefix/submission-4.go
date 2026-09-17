e4033a65311985d26e4851d299ef43f60fe8a7be
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-09-17 12:50:11 +0530

    Add: longest-common-prefix - submission-4

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-4.go
@@ -0,0 +1,14 @@
+func longestCommonPrefix(strs []string) string {
+	prefix := strs[0]
+    for i := 1; i < len(strs); i++ {
+		j := 0
+        for j < len(prefix) && j < len(strs[i]) && prefix[j] == strs[i][j] {
+			j++
+		}
+        prefix = prefix[:j]
+        if prefix == "" {
+			return ""
+		}
+	}
+    return prefix
+}
