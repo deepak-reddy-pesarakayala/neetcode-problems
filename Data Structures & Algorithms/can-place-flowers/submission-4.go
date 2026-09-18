a92d87b2b9edc2a04650ce2adee9e6fc6e13e8af
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-09-18 12:20:32 +0530

    Add: can-place-flowers - submission-4

---
+++ Data Structures & Algorithms/can-place-flowers/submission-4.go
@@ -0,0 +1,20 @@
+func canPlaceFlowers(flowerbed []int, n int) bool {
+	count := 0
+
+	for i := 0; i < len(flowerbed); i++ {
+		if flowerbed[i] == 0 {
+			leftEmpty := i == 0 || flowerbed[i-1] == 0
+			rightEmpty := i == len(flowerbed)-1 || flowerbed[i+1] == 0
+
+			if leftEmpty && rightEmpty {
+				flowerbed[i] = 1
+				count++
+
+				if count >= n {
+					return true
+				}
+			}
+		}
+	}
+    return count >= n
+}
