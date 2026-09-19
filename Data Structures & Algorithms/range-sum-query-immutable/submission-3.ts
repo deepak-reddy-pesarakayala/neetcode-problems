df18e35a8d80a3a13964309fdd7e316827100e48
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-09-19 15:16:32 +0530

    Add: range-sum-query-immutable - submission-3

---
+++ Data Structures & Algorithms/range-sum-query-immutable/submission-3.ts
@@ -0,0 +1,14 @@
+class NumArray {
+    prefix: number[];
+
+    constructor(nums: number[]) {
+        this.prefix = new Array(nums.length + 1).fill(0);
+        for (let i = 0; i < nums.length; i++) {
+            this.prefix[i + 1] = this.prefix[i] + nums[i];
+        }
+    }
+
+    sumRange(left: number, right: number): number {
+        return this.prefix[right + 1] - this.prefix[left];
+    }
+}
