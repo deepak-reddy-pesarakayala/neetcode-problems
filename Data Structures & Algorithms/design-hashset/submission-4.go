31cf289c5c671afc0b34502d78cc72064a6240ec
Deepak Reddy Pesarakayala <pesarakayaladeepakreddy@gmail.com>
2026-09-20 20:30:09 +0530

    Add: design-hashset - submission-4

---
+++ Data Structures & Algorithms/design-hashset/submission-4.go
@@ -0,0 +1,21 @@
+type MyHashSet struct {
+	data []bool
+}
+
+func Constructor() MyHashSet {
+	return MyHashSet{
+		data: make([]bool, 1000001),
+	}
+}
+
+func (this *MyHashSet) Add(key int) {
+	this.data[key] = true
+}
+
+func (this *MyHashSet) Remove(key int) {
+	this.data[key] = false
+}
+
+func (this *MyHashSet) Contains(key int) bool {
+	return this.data[key]
+}
